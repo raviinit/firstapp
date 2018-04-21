7ec8b57462da5238fea801f7556e8576e8af6265
raviint <raviinit@ymail.com>
2018-04-21 13:57:08 +0530

    Initial commit

---
+++ app/src/main/java/com/first/local/firstapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.first.local.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void doSomething(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
